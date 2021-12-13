a7b82b769c27b3cb94964552f90d0f7e3bc76ac4
renatabiniek <renata.biniek@gmail.com>
2021-12-13 22:57:57 +0000

    Fix error with missing button type

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ function getContactDetails(event) {
    let inputContactName = contactName.value;
    let inputEmail = email.value;
 
-   let message = `Thank you for your message, ${inputContactName}!</br>We'll get back to you at ${inputEmail} shortly!`;
+   let message = `Thank you for your message, ${inputContactName}!<br>We'll get back to you at ${inputEmail} shortly!`;
 
    document.getElementById("thank-you").innerHTML = message;
    resetForm();
